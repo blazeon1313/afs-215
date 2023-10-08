cf8cb4104d990ea96c39c3912c9da795a0833985
Kristoffer Krueger <kristoffer.krueger@bryanuniversity.edu>
2023-10-07 21:10:09 -0600

    week2

---
+++ week2/assignments/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,19 @@
+def pepsicoke(num):
+    if ((num % 3 == 0) and (num % 5 == 0)):
+        return 'PepsiCoke'
+    else:
+        if num == 1:
+            return 1
+        elif num == 2:
+            return 2
+        elif num % 3 == 0:
+            return 'Pepsi'
+        elif num % 5 == 0:
+            return 'Coke'
+
+
+pepsicoke(1)
+pepsicoke(2)
+pepsicoke(3)
+pepsicoke(5)
+pepsicoke(15)
---
+++ week2/assignments/fizzbuzz/test_fizzbuzz.py
@@ -0,0 +1,51 @@
+from fizzbuzz import pepsicoke
+
+
+# can Call on PepsiCoke()
+# get "1" when passed a 1
+def test_one():
+    soda = pepsicoke(1)
+    print(soda)
+    assert soda == 1
+
+
+# get "2" when passed a 2
+def test_two():
+    soda = pepsicoke(2)
+    print(soda)
+    assert soda == 2
+
+
+# get Pepsi to print when passed a 3
+def test_three():
+    soda = pepsicoke(3)
+    print(soda)
+    assert soda == 'Pepsi'
+
+
+# get Coke to print whem passed a 5
+def test_four():
+    soda = pepsicoke(5)
+    print(soda)
+    assert soda == 'Coke'
+
+
+# get Pepsi when passed a 6(a multiple of 3)
+def test_five():
+    soda = pepsicoke(6)
+    print(soda)
+    assert soda == 'Pepsi'
+
+
+# get Coke when passed a 10(multiple of 5)
+def test_six():
+    soda = pepsicoke(10)
+    print(soda)
+    assert soda == 'Coke'
+
+
+# get PepsiCoke when passed a 15(multiple of 3 and 5)
+def test_seven():
+    soda = pepsicoke(15)
+    print(soda)
+    assert soda == 'PepsiCoke'
